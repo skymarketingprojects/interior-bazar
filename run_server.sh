28208e204957fcfaeb07fc52b78b4525c46b3717
Nikhil-gusain <nikstudyc03@gmail.com>
2025-09-12 21:53:13 +0530

    proc and sh updated

--- run_server.sh
+++ run_server.sh
@@ -26,4 +26,5 @@ python manage.py migrate
 
 # Start the server
 echo "Starting server on http://127.0.0.1:8888/"
-python -m uvicorn interior_bazzar.asgi:application --port 8888 --host 0.0.0.0
+# python -m uvicorn interior_bazzar.asgi:application --port 8888 --host 0.0.0.0
+uvicorn interior_bazzar.asgi:application --host 0.0.0.0 --port 8000
