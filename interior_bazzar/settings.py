c1a7d255e18d5d8edb249a8972f494c1aaf08c37
Nikhil-gusain <nikstudyc03@gmail.com>
2025-09-12 22:07:13 +0530

    proc and sh not worked

--- interior_bazzar/settings.py
+++ interior_bazzar/settings.py
@@ -23,7 +23,7 @@ os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DJANGO_ENV = os.environ.get("ENV", APPMODE.LOC).lower()
+DJANGO_ENV = os.environ.get("ENV", APPMODE.LOC)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
