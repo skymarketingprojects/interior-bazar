a4eb1e6ff43feab45dcaad177f7a6c89982399f1
Nikhil-gusain <nikstudyc03@gmail.com>
2025-09-12 12:11:52 +0530

    dev

--- interior_bazzar/settings.py
+++ interior_bazzar/settings.py
@@ -207,32 +207,11 @@ SIMPLE_JWT = {
 
 if(ENV == APPMODE.DEV):
     print(f'Mode Activated [ DEV ]')
-    # DEBUG = True 
-
-    # # Razor Pay
-    # RAZORPAY_KEY = os.getenv('RAZORPAY_KEY')
-    # RAZORPAY_KEY_SECRET = os.getenv('RAZORPAY_KEY_SECRET')
-
-    # ALLOWED_HOSTS = ["*"]
-
-    # # CORS Settings (uncomment if needed)
-    # CORS_ORIGIN_ALLOW_ALL = True
-
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #         'NAME': os.getenv('DATABASE_NAME'),
-    #         'USER': os.getenv('DATABASE_USERNAME'),
-    #         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-    #         'HOST': os.getenv('DATABASE_HOST'),
-    #         'PORT': os.getenv('DATABASE_PORT'),
-    #     }
-    # }
     DEBUG = True 
         
     # Razor Pay
-    # RAZORPAY_KEY=env("PROD_RAZORPAY_KEY")
-    # RAZORPAY_KEY_SECRET=env("PROD_RAZORPAY_KEY_SECRET")
+    RAZORPAY_KEY=env("PROD_RAZORPAY_KEY")
+    RAZORPAY_KEY_SECRET=env("PROD_RAZORPAY_KEY_SECRET")
     
 
     ALLOWED_HOSTS = ["*"]
@@ -358,12 +337,18 @@ STATIC_ROOT = BASE_DIR  / 'static_root'
 # Email Server
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use generated app password here
+# EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use generated app password here
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')  # Use generated app password here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
     # Development:
     #  eb deploy dev-ib-env-1 --profile interior_bazzar_home
