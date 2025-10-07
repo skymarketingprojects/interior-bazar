6e3bdcb18b1e1c0c8e148163a248f3113692fc9b
Nikhil-gusain <nikstudyc03@gmail.com>
2025-10-07 21:58:40 +0530

    requirement.config added for phone pay sdk

--- .platform/hooks/prebuild/01_install_dependencies.sh
+++ .platform/hooks/prebuild/01_install_dependencies.sh
@@ -8,3 +8,14 @@ sudo yum install -y postgresql-devel
 
 # Install other necessary build tools
 sudo yum install -y gcc
+
+#!/bin/bash
+
+# Activate the EB virtual environment
+source /var/app/venv/*/bin/activate
+
+# Install PhonePe SDK into that venv
+pip install \
+  --index-url https://phonepe.mycloudrepo.io/public/repositories/phonepe-pg-sdk-python \
+  --extra-index-url https://pypi.org/simple \
+  phonepe_sdk
