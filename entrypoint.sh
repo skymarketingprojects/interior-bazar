eeefe25466c313652d8dcc404f53ab95aa002259
Nikhil-gusain <nikstudyc03@gmail.com>
2025-12-08 14:45:11 +0530

    vps hosting changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # Start Gunicorn
-exec gunicorn interior_bazzar_api.wsgi:application \
+exec gunicorn interior_bazzar.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3 \
     --log-level info
